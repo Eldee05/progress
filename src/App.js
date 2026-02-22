56e64ba9525372ffb97da4084a900ae960e29960
Eldee05 <eldivinesolace@gmail.com>
2026-02-22 03:00:52 +0100

    stepper

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+export default App;
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 
 function App() {
+  const stepper = 1;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="steps">
+      <div className="numbers">
+        <div className="active">1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+
+      <p className="message">
+        stepper {stepper} : {messages[stepper - 1]}
+      </p>
+
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950ff", color: "#ffffff" }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950ff", color: "#ffffff" }}>
+          Next
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
